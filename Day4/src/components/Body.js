076b76a34f03ce2c3367fd2affee3080725f0fab
Tushar Kumar <tusharkumar19701@gmail.com>
2024-03-06 18:20:55 +0530

    Merge branch 'main' of https://github.com/tusharkumar19701/ReactCodes

--- Day4/src/components/Body.js
+++ Day4/src/components/Body.js
@@ -1,32 +1,54 @@
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react"; // We import it using a named variable
-
+import Shimmer from "./Shimmer";
 
 
 function filterData(searchText, restaurants) {
   const filterData =  restaurants.filter((restaurant) => 
-    restaurant.info.name.includes(searchText)
+    restaurant?.info?.name?.toLowerCase()?. includes(searchText.toLowerCase())
   );
   return filterData;
 }
 
-
 const Body = () => {
   //searchText is a local state variable
   const [searchText, setSearchText] = useState(""); // To create state variables  - it returns [variable name, function to update the variable]
+
+  // const [allRestaurants, setAllRestaurants] = useState([]);
+
   
   // useEffect is a hook - we call this function giving two parameters - one is the callback func. and the other is the dependency array
   // this callback func is called on every re-render,but if you dont want to call it again and again so give an empty dependency array to call it just once
 
   //suppose you want to call this useEffect only when the searchText changes then write searchText in the dependency array 
-  useEffect(() => {
 
+  //empty dependency array => will be called just once and after render
+  //dep array -> [searchText] => will be called once after initial render + everytime after my searchText changes
+  useEffect(() => {
+    getRestaurants();
   }, []);
 
-  const [restaurants,setRestaurants] = useState(restaurantList);
+  async function getRestaurants() {
+    try {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.29844139999999&lng=77.99313599999999&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+  
+      //Optional chaining  -> ?
+      // setAllRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+      setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch(err) {
+      console.log(err);
+    }
+  }
 
-  return (
+  const [allRestaurants,setRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+
+  if(!allRestaurants) return null; 
+
+  return (allRestaurants?.length === 0) ? <Shimmer /> : (
     <>
       <div className="search-container">
         <input
@@ -41,16 +63,16 @@ const Body = () => {
         />
         <button className="search-btn" onClick={() => {
           //need to filter the data
-          const data = filterData(searchText, restaurants);
+          const data = filterData(searchText, allRestaurants);
           //update the state - restaurants
-          setRestaurants(data);
+          setFilteredRestaurants(data);
         }}>Search</button>
 
       </div>
       <div className="restaurant-list">
-        {restaurants.map((restaurant) => {
+        {filteredRestaurants.map((restaurant) => {
           return (
-            <RestaurantCard {...restaurant.info} key={restaurant.info.id} />
+            <RestaurantCard {...restaurant?.info} key={restaurant?.info?.id} />
           );
         })}
       </div>
